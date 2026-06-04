a2f60ff8674d94481890cee9153dcca803b3369a
Chentsov <Math@chentsovfedor.ru>
2026-06-04 09:26:21 +0300

    9:26

--- methods/main.cpp
+++ methods/main.cpp
@@ -2,7 +2,7 @@
  * @file methods/main.cpp
  * @author Mikhail Lozhnikov
  *
- * Файл с функией main() для серверной части программы.
+ * Файл с функцией main() для серверной части программы.
  */
 
 #include <httplib.h>
@@ -15,123 +15,82 @@
 
 using json = nlohmann::json;
 
-// mm::TasksQueue tasksQueue;
-// Делаю глобальной переменной, чтоб HeatEquationMethod
-// имел доступ к очереди задач
-
 int main(int argc, char* argv[]) {
-  // Порт по-умолчанию.
-  int port = 8080;
+    // Порт по-умолчанию.
+    int port = 8080;
 
-  if (argc >= 2) {
-    // Меняем порт по умолчанию, если предоставлен соответствующий
-    // аргумент командной строки.
-    if (std::sscanf(argv[1], "%d", &port) != 1)
-      return -1;
-  }
-
-  std::cerr << "Listening on port " << port << "..." << std::endl;
-
-  httplib::Server svr;
-
-  mm::TasksQueue tasksQueue;
-
-  // Обработчик для GET запроса по адресу /stop. Этот обработчик
-  // останавливает сервер.
-  svr.Get("/stop", [&](const httplib::Request&, httplib::Response&) {
-    svr.stop();
-  });
-
-
-  svr.Post("/CheckTaskStatus", [&](const httplib::Request& req,
-                                        httplib::Response& res) {
-    /*
-    Поле body структуры httplib::Request содержит текст запроса.
-    Функция nlohmann::json::parse() используется для того,
-    чтобы преобразовать текст в объект типа nlohmann::json.
-    */
-    nlohmann::json input = nlohmann::json::parse(req.body);
-    nlohmann::json output;
-
-    int taskId = input["id"];
-
-    output["id"] = taskId;
-
-    if (tasksQueue.IsTaskFinished(taskId)) {
-      // Задача завершена можно скачивать данные.
-      output["status"] = "finished";
-    } else {
-      /* Задача либо не была добавлена, либо она ещё не досчиталась,
-       * либо она уже посчитана, и данные уже скачаны и удалены с сервера. */
-      output["status"] = "unknown";
+    if (argc >= 2) {
+        // Меняем порт по умолчанию, если предоставлен соответствующий
+        // аргумент командной строки.
+        if (std::sscanf(argv[1], "%d", &port) != 1)
+            return -1;
     }
 
-    /*
-    Метод nlohmann::json::dump() используется для сериализации
-    объекта типа nlohmann::json в строку. Метод set_content()
-    позволяет задать содержимое ответа на запрос. Если передаются
-    JSON данные, то MIME тип следует выставить application/json.
-    */
-    res.set_content(output.dump(), "application/json");
-  });
+    std::cerr << "Listening on port " << port << "..." << std::endl;
 
+    httplib::Server svr;
+    mm::TasksQueue tasksQueue;
 
-  svr.Post("/DownloadTaskData", [&](const httplib::Request& req,
-                                        httplib::Response& res) {
-    /*
-    Поле body структуры httplib::Request содержит текст запроса.
-    Функция nlohmann::json::parse() используется для того,
-    чтобы преобразовать текст в объект типа nlohmann::json.
-    */
-    nlohmann::json input = nlohmann::json::parse(req.body);
-    nlohmann::json output;
+    // Обработчик для GET запроса по адресу /stop.
+    svr.Get("/stop", [&](const httplib::Request&, httplib::Response& res) {
+        svr.stop();
+        res.set_content("Stopped", "text/plain");
+        });
 
-    int taskId = input["id"];
+    svr.Post("/CheckTaskStatus", [&](const httplib::Request& req,
+        httplib::Response& res) {
+            nlohmann::json input;
+            try {
+                input = nlohmann::json::parse(req.body);
+            }
+            catch (...) {
+                input = nlohmann::json::object();
+            }
 
-    if (tasksQueue.IsTaskFinished(taskId)) {
-      // Задача завершена можно скачивать данные.
-      output = tasksQueue.GetFinishedTaskData(taskId);
-    } else {
-      /* Задача либо не была добавлена, либо она ещё не досчиталась,
-       * либо она уже посчитана, и данные уже скачаны и удалены с сервера. */
-      output["status"] = "unknown";
-    }
+            nlohmann::json output;
+            int taskId = input.value("id", 0);
+            output["id"] = taskId;
 
-    output["id"] = taskId;
+            if (tasksQueue.IsTaskFinished(taskId)) {
+                output["status"] = "finished";
+            }
+            else {
+                output["status"] = "unknown";
+            }
 
-    /*
-    Метод nlohmann::json::dump() используется для сериализации
-    объекта типа nlohmann::json в строку. Метод set_content()
-    позволяет задать содержимое ответа на запрос. Если передаются
-    JSON данные, то MIME тип следует выставить application/json.
-    */
-    res.set_content(output.dump(), "application/json");
-  });
+            res.set_content(output.dump(), "application/json");
+        });
 
+    svr.Post("/DownloadTaskData", [&](const httplib::Request& req,
+        httplib::Response& res) {
+            nlohmann::json input = nlohmann::json::parse(req.body);
+            nlohmann::json output;
+            int taskId = input["id"];
 
+            if (tasksQueue.IsTaskFinished(taskId)) {
+                output = tasksQueue.GetFinishedTaskData(taskId);
+            }
+            else {
+                output["status"] = "unknown";
+            }
 
+            output["id"] = taskId;
+            res.set_content(output.dump(), "application/json");
+        });
 
-  /* Сюда нужно вставить обработчик post запроса для алгоритма. */
-  svr.Post("/HeatEquation", [&](const httplib::Request& req,
-  httplib::Response& res) {
-    nlohmann::json input = nlohmann::json::parse(req.body);
-    nlohmann::json output;
-    int taskId = mm::HeatEquationMethod(input, &output,
-                                        tasksQueue);
-    output["id"] = taskId;
-    res.set_content(output.dump(), "application/json");
-  });
-  // Вызывает метод POST у нашего сервера svr
-  // "/HeatEquation" -- путь, по которому принимает POST-запрос
-  // лямбда-функция для запуска задачи и получения результата по id
+    /* Сюда нужно вставить обработчик post запроса для алгоритма. */
+    svr.Post("/HeatEquation", [&](const httplib::Request& req,
+        httplib::Response& res) {
+            nlohmann::json input = nlohmann::json::parse(req.body);
+            nlohmann::json output;
+            int taskId = mm::HeatEquationMethod(input, &output, tasksQueue);
+            output["id"] = taskId;
+            res.set_content(output.dump(), "application/json");
+        });
+    /* Конец вставки. */
 
+    // Эта функция запускает сервер на указанном порту.
+    svr.listen("0.0.0.0", port);
 
-
-  /* Конец вставки. */
-
-  // Эта функция запускает сервер на указанном порту. Программа не завершится
-  // до тех пор, пока сервер не будет остановлен.
-  svr.listen("0.0.0.0", port);
-
-  return 0;
+    return 0;
 }
--- methods/tasks_queue.cpp
+++ methods/tasks_queue.cpp
@@ -9,103 +9,97 @@
 
 namespace mm {
 
-using std::chrono::steady_clock;
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
+    using std::chrono::steady_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
 
-TasksQueue::TasksQueue() :
-  lastTaskId(0),
-  finished(false),
-  queueThread([&]() { return this->ThreadFunction(); } ) {
-}
-
-
-TasksQueue::~TasksQueue() {
-  {
-    std::unique_lock locker(m);
-    finished = true;
-    condvar.notify_one();
-  }
-  queueThread.join();
-}
-
-int TasksQueue::AddTask(AbstractSolverWrapper* task) {
-  std::unique_lock locker(m);
-
-  tasks.emplace(++lastTaskId, task);
-
-  condvar.notify_one();
-
-
-  return lastTaskId;
-}
-
-bool TasksQueue::IsTaskFinished(int id) {
-  std::unique_lock locker(m);
-
-  return finishedTasksData.find(id) != finishedTasksData.end();
-}
-
-nlohmann::json TasksQueue::GetFinishedTaskData(int id) {
-  std::unique_lock locker(m);
-
-  nlohmann::json data = finishedTasksData.at(id);
-
-  finishedTasksData.erase(id);
-
-  return data;
-}
-
-void TasksQueue::ThreadFunction() {
-  while (true) {
-    std::pair<int, AbstractSolverWrapper*> ticket;
-
-    {
-      std::unique_lock locker(m);
-
-      if (finished)
-        return;
-
-      if (tasks.empty()) {
-        condvar.wait(locker);
-        continue;
-      }
-
-      ticket = tasks.back();
-
-      tasks.pop();
+    TasksQueue::TasksQueue() :
+        lastTaskId(0),
+        finished(false),
+        queueThread([&]() { this->ThreadFunction(); }) {
     }
 
-    auto [currentTaskId, solverWrapper] = ticket;
-
-    nlohmann::json data;
-
-    auto start = steady_clock::now();
-
-    bool status = solverWrapper->Solve(&data);
-
-    auto finish = steady_clock::now();
-
-    delete solverWrapper;
-
-    double elapsed = duration_cast<milliseconds>(finish - start).count() /
-        1000.0;
-
-    if (status) {
-      data["status"] = "ok";
-      data["time"] = elapsed;
-    } else {
-      data["status"] = "failed";
+    TasksQueue::~TasksQueue() {
+        {
+            std::unique_lock<std::mutex> locker(m);
+            finished = true;
+            condvar.notify_one();
+        }
+        queueThread.join();
     }
 
-    std::unique_lock locker(m);
+    int TasksQueue::AddTask(AbstractSolverWrapper* task) {
+        std::unique_lock<std::mutex> locker(m);
 
-    finishedTasksData[currentTaskId] = data;
-  }
-}
+        tasks.emplace(++lastTaskId, task);
 
+        condvar.notify_one();
+
+        return lastTaskId;
+    }
+
+    bool TasksQueue::IsTaskFinished(int id) {
+        std::unique_lock<std::mutex> locker(m);
+
+        return finishedTasksData.find(id) != finishedTasksData.end();
+    }
+
+    nlohmann::json TasksQueue::GetFinishedTaskData(int id) {
+        std::unique_lock<std::mutex> locker(m);
+
+        nlohmann::json data = finishedTasksData.at(id);
+
+        finishedTasksData.erase(id);
+
+        return data;
+    }
+
+    void TasksQueue::ThreadFunction() {
+        while (true) {
+            std::pair<int, AbstractSolverWrapper*> ticket;
+
+            {
+                std::unique_lock<std::mutex> locker(m);
+
+                if (finished)
+                    return;
+
+                if (tasks.empty()) {
+                    condvar.wait(locker);
+                    continue;
+                }
+
+                ticket = tasks.back();
+                tasks.pop();
+            }
+
+            auto [currentTaskId, solverWrapper] = ticket;
+
+            nlohmann::json data;
+
+            auto start = steady_clock::now();
+
+            bool status = solverWrapper->Solve(&data);
+
+            auto finish = steady_clock::now();
+
+            delete solverWrapper;
+
+            double elapsed = duration_cast<milliseconds>(finish - start).count() /
+                1000.0;
+
+            if (status) {
+                data["status"] = "ok";
+                data["time"] = elapsed;
+            }
+            else {
+                data["status"] = "failed";
+            }
+
+            std::unique_lock<std::mutex> locker(m);
+
+            finishedTasksData[currentTaskId] = data;
+        }
+    }
 
 }  // namespace mm
-
-
-
